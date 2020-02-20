d2d8abfcaf9a22e2a254702651efd36fe8e2f291
dhphat <dhphat@outlook.com>
2020-02-20 10:15:29 +0700

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -5,7 +5,7 @@ export const products = [
     description: 'A large phone with one of the best screens'
   },
   {
-    name: 'Phone Mini',
+    name: 'Phone Mini Haha',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
